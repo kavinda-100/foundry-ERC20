87e383cffbc6645b0cae4844a4a5f45df5040a64
kavinda-100 <kavindarathnayake100@gmail.com>
2025-05-01 09:54:21 +0530

    feat: add OpenZeppelinToken contract with ERC20 implementation

--- src/OpenZeppelinToken.sol
+++ src/OpenZeppelinToken.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-contract OpenZeppelinToken {}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OpenZeppelinToken is ERC20 {
+    constructor(uint256 _initialSupply) ERC20("OpenZeppelinToken", "OZT") {
+        _mint(msg.sender, _initialSupply);
+    }
+}
