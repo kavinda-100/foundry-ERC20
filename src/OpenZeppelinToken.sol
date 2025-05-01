7cba96b002d127d289e2aafcd282ef276d64f064
kavinda-100 <kavindarathnayake100@gmail.com>
2025-05-01 10:00:16 +0530

    refactor: update import statement for ERC20 in OpenZeppelinToken contract

--- src/OpenZeppelinToken.sol
+++ src/OpenZeppelinToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract OpenZeppelinToken is ERC20 {
     constructor(uint256 _initialSupply) ERC20("OpenZeppelinToken", "OZT") {
